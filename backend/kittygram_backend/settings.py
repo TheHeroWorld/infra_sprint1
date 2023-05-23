8a2b50fed363cc9bef9987b2ab0ecf4839e08611
Denis <blomy@yandex.ru>
2023-05-23 15:31:46 +0000

    f

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -10,7 +10,7 @@ SECRET_KEY = "django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.65.78', '127.0.0.1', 'localhost', 'kittyflu.hopto.org']
+ALLOWED_HOSTS = ['158.160.65.78', '127.0.0.1', 'localhost', 'kittygramhero.hopto.org']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
