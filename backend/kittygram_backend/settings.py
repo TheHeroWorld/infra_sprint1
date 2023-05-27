35746244cd21fa80a61161582dce16fbb131beca
Denis <blomy@yandex.ru>
2023-05-27 10:59:42 +0400

    f

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
-source venv/Scripts/activate
 ALLOWED_HOSTS = ['158.160.65.78', '127.0.0.1', 'localhost', 'kittygramhero.hopto.org']
 
 INSTALLED_APPS = [
