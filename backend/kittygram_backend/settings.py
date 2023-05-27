1f9a42b97389c3c3fa352c478920918cc403f22b
Denis <blomy@yandex.ru>
2023-05-27 10:47:00 +0400

    f

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -6,10 +6,10 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = "django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = False
-
+source venv/Scripts/activate
 ALLOWED_HOSTS = ['158.160.65.78', '127.0.0.1', 'localhost', 'kittygramhero.hopto.org']
 
 INSTALLED_APPS = [
